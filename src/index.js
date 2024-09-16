2ab8ee88374db64817fa6694f079d2f7a2533b9f
RisaMuraguchi <126639980+RisaMuraguchi@users.noreply.github.com>
2024-09-16 17:27:45 +0900

    3-1コンポーネント

--- src/index.js
+++ src/index.js
@@ -1,19 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+// import './chap02/class.css';
 // import App from './App';
 import reportWebVitals from './reportWebVitals';
+// import MyHello from './chap03/Myhello';
+import EventBasic from './chap03/EventBasic';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-const content = `<h3>WINGSプロジェクト</h3>
-  <img src="https://wings.msn.to/image/wings.jpg" />`
   root.render(
-      <>
-        <p dangerouslySetInnerHTML={{__html: content}}></p>
-      </>
+    <EventBasic type="d" />
   );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+
 reportWebVitals(console.log);
